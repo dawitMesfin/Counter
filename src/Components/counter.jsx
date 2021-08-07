c863874aaca1455280626d10721fc7502cdc79ce
Dawit Mesfin <dawitmesfin2019@gmail.com>
2021-08-07 11:53:27 +0300

    modified with state

--- src/Components/counter.jsx
+++ src/Components/counter.jsx
@@ -1,28 +1,39 @@
 import React, { Component, Fragment } from "react";
 
 class Counter extends Component {
+  constructor() {
+    super();
+    this.handleIncreament = this.handleIncreament.bind(this);
+  }
   state = {
-    count: 98,
-    tags: ["tag1", "tag2", "tag3"],
+    count: 9,
   };
 
-  tagRender() {
-    if (this.state.tags.length == 0) return <p>there is no tag daveye</p>;
-    return (
-      <ul>
-        {this.state.tags.map((tag) => (
-          <li key={tag}>{tag}</li>
-        ))}
-      </ul>
-    );
+  formatCount() {
+    return this.state.count == 0 ? "zero" : this.state.count;
+  }
+
+  getBadge() {
+    let classes = "badge m-5 badge-";
+    classes += this.state.count == 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  handleIncreament() {
+    this.setState({ count: this.state.count + 1 });
   }
 
   render() {
     return (
-      <React.Fragment>
-        {this.state.tags.length == 0 && "Empty tag dava"}
-        {this.tagRender()}
-      </React.Fragment>
+      <div>
+        <span className={this.getBadge()}>{this.formatCount()}</span>
+        <button
+          onClick={this.handleIncreament}
+          className="btn btn-secondary btn-sm"
+        >
+          Increament
+        </button>
+      </div>
     );
   }
 }
